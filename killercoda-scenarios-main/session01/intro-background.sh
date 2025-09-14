7183bff6d24209337dd26f4445ce6d1abbfbd8ec
karimdjaafar <kdjaafar@jasmineconseil.com>
2025-09-14 18:32:28 +0000

    update

--- killercoda-scenarios-main/session01/intro-background.sh
+++ killercoda-scenarios-main/session01/intro-background.sh
@@ -2,8 +2,8 @@
 
 set -x   # Output is in /var/log/killercoda/background0_std*
 
+apt -y install openjdk-21-jre-headless
+
 wget -O data.tar.gz https://zenodo.org/record/3997237/files/FAIR_Bioinfo_data.tar.gz?download=1
 tar -xzf data.tar.gz
 rm data.tar.gz
-
-
